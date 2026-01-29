7049147eccd5e6aeaeb10e07c335393cd57e20e6
Itay Etelis <itay.etelis@ibm.com>
2026-01-29 16:59:44 +0200

    Remove Neural Magic copyright headers from source files
    Strip the Neural Magic copyright and license headers from all Python
    files in src/, tests/, and examples/ directories as part of the
    project transition to the vLLM organization.
    
    Signed-off-by: Itay Etelis <itay.etelis@ibm.com>

--- src/compressed_tensors/compressors/model_compressors/model_compressor.py
+++ src/compressed_tensors/compressors/model_compressors/model_compressor.py
@@ -1,17 +1,3 @@
-# Copyright (c) 2021 - present / Neuralmagic, Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import json
 import logging
 import operator
@@ -339,10 +325,10 @@ class ModelCompressor:
 
             self.quantization_compressor = {}
             for format in self.compression_formats:
-                self.quantization_compressor[
-                    format
-                ] = BaseCompressor.load_from_registry(
-                    format, config=quantization_config
+                self.quantization_compressor[format] = (
+                    BaseCompressor.load_from_registry(
+                        format, config=quantization_config
+                    )
                 )
 
     def get_missing_module_keys(self, model: Module) -> list[str]:
