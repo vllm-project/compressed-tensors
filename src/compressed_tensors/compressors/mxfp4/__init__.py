e566240776b1e7fcdd5350f05742bdf567120443
Kyle Sayers <kylesayrs@gmail.com>
2026-03-09 15:41:07 -0400

    Refactor compressors
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/compressed_tensors/compressors/mxfp4/__init__.py
+++ src/compressed_tensors/compressors/mxfp4/__init__.py
@@ -2,7 +2,5 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 # flake8: noqa
+
 from .base import *
-from .dense import *
-from .sparse_24_bitmask import *
-from .sparse_bitmask import *
--- src/compressed_tensors/compressors/nvfp4/__init__.py
+++ src/compressed_tensors/compressors/nvfp4/__init__.py
@@ -2,7 +2,6 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
 # flake8: noqa
+
 from .base import *
-from .dense import *
-from .sparse_24_bitmask import *
-from .sparse_bitmask import *
+from .helpers import *
