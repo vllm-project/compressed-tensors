71d4b2b81e09e46a753649e92a795f42a7ffee00
Brian Dellabetta <bdellabe@redhat.com>
2026-03-13 18:25:11 +0000

    update example based on changes
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- examples/convert_checkpoint/deepseek32_fpblock_example.py
+++ examples/convert_checkpoint/deepseek32_fpblock_example.py
@@ -5,10 +5,11 @@ from compressed_tensors.entrypoints.convert import (
     convert_checkpoint,
     FP8BlockToBfloat16Converter,
 )
-from compressed_tensors.modeling.deepseekv32.model import (
-    Transformer as DeepseekV32ForCausalLM,
-    ModelArgs as Deepseek32Config,
+from compressed_tensors.modeling.deepseekv32.model import DeepseekV32ForCausalLM
+from compressed_tensors.modeling.deepseekv32.config import (
+    ModelConfig as Deepseek32Config,
 )
+
 from llmcompressor.entrypoints.model_free.reindex_fused_weights import (
     reindex_fused_weights,
 )
@@ -23,17 +24,17 @@ SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-bf16"
 
 # Some weight and weight_scale_inv tensors are split across safetensors files
 reindex_fused_weights(model_stub=MODEL_ID, save_directory=SAVE_DIR)
-# Convert Qwen3-4B-FP8 back to dense bfloat16 format
+# Convert DeepSeek-V3.2 back to dense bfloat16 format
 convert_checkpoint(
     model_stub=SAVE_DIR,
     save_directory=SAVE_DIR,
     converter=FP8BlockToBfloat16Converter(
-        # qwen-community/Qwen3-4B-FP8's fp8-block-quantized layers, found by inspection
+        # DeepSeek-V3.2's fp8-block-quantized layers, found by inspection
         targets=[
-            "re:.*mlp.*\.(gate_up|gate|up|down)_proj$",
-            "re:.*self_attn.*\.(kv_b|o|q_a|q_b)_proj$",
-            "re:.*self_attn.kv_a_proj_with_mqa$",
-            "re:.*self_attn.indexer.(wk|wq_b)$",
+            r"re:.*mlp.*\.(gate_up|gate|up|down)_proj$",
+            r"re:.*self_attn.*\.(kv_b|o|q_a|q_b)_proj$",
+            r"re:.*self_attn.kv_a_proj_with_mqa$",
+            r"re:.*self_attn.indexer.(wk|wq_b)$",
         ],
         weight_block_size=[128, 128],
     ),
