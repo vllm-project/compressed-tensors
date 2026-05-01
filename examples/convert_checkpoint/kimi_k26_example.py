001e9339fab17002d78169c6319d49cdb5f10ca4
Brian Dellabetta <bdellabe@redhat.com>
2026-05-01 17:38:24 +0000

    clean up example
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- examples/convert_checkpoint/kimi_k26_example.py
+++ examples/convert_checkpoint/kimi_k26_example.py
@@ -9,11 +9,9 @@ from compressed_tensors.entrypoints.convert import (
 # moonshotai/Kimi-K2.6 checkpoint is published in compressed-tensors format.
 # This script will upconvert to bfloat16 so that the model can be compressed
 # in another configuration.
-# MODEL_ID = "/mnt/data/engine/brian-dellabetta/Qwen3.5-35B-A3B-W8A8-INT8"
 MODEL_ID = "moonshotai/Kimi-K2.6"
 SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-bf16"
 
-# Convert DeepSeek-V3.2 back to dense bfloat16 format
 convert_checkpoint(
     model_stub=MODEL_ID,
     save_directory=SAVE_DIR,
