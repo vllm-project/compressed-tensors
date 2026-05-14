20524c493bda139722df84deea0b94338efe76e3
Dan Huang <dahuang@redhat.com>
2026-05-14 10:54:59 -0400

    use 2 gpus and add HF_HOME
    Signed-off-by: Dan Huang <dahuang@redhat.com>

--- .buildkite/scripts/test-check.sh
+++ .buildkite/scripts/test-check.sh
@@ -3,9 +3,9 @@
 cat /etc/issue
 
 # install uv/compilers and check env
-#apk add --no-cache curl build-base
 apt-get update && apt-get install -y curl g++ gcc make
 curl -LsSf https://astral.sh/uv/install.sh | env UV_VERSION=0.8.15 sh
+
 export LD_LIBRARY_PATH=/usr/local/nvidia/lib64
 export PATH="$HOME/.local/bin:/usr/local/nvidia/bin:$PATH"
 nvidia-smi
@@ -16,5 +16,7 @@ uv venv testvenv --python 3.12
 source testvenv/bin/activate
 
 export UV_TORCH_BACKEND=cu130
+export HF_HOME=/model-cache
 uv pip install .[dev] --index-strategy unsafe-best-match --extra-index-url https://download.pytorch.org/whl/cu130
+
 make test
