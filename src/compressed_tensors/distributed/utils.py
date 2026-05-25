6b0b60b8cfc3085a5d8b66d032381e167fa82ede
yiliu30 <yi4.liu@intel.com>
2026-05-24 21:57:28 -0700

    fix backend
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- src/compressed_tensors/distributed/utils.py
+++ src/compressed_tensors/distributed/utils.py
@@ -64,10 +64,19 @@ def init_dist():
     local_rank = int(os.environ["LOCAL_RANK"])
     world_size = int(os.environ["WORLD_SIZE"])
 
-    device = torch.device(f"cuda:{local_rank}")
-    torch.cuda.set_device(device)
+    accel_type = torch.accelerator.current_accelerator().type
+    device = torch.device(f"{accel_type}:{local_rank}")
+    torch.accelerator.set_device_index(local_rank)
+
+    if accel_type == "cuda":
+        backend = "nccl"
+    elif accel_type == "xpu":
+        backend = "xccl"
+    else:
+        backend = "gloo"
+
     dist.init_process_group(
-        backend="nccl",
+        backend=backend,
         init_method="env://",
         rank=rank,
         world_size=world_size,
