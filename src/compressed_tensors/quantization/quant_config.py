2cff3441b2f0a691f62b659ee244ca22f20c7b1a
Brian Dellabetta <brian-dellabetta@users.noreply.github.com>
2026-07-29 10:32:03 -0400

    [QuantizationConfig] drop propagation of quant config format to scheme format (#799)
    * drop propagation of quant config format to scheme format
    
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>
    
    * style
    
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>
    
    ---------
    
    Signed-off-by: Brian Dellabetta <bdellabe@redhat.com>

--- src/compressed_tensors/quantization/quant_config.py
+++ src/compressed_tensors/quantization/quant_config.py
@@ -178,16 +178,6 @@ class QuantizationConfig(BaseModel):
                 targets=targets_or_scheme,
             )
 
-        # if unset, populate format on each config group
-        if self.format not in (
-            DEFAULT_QUANTIZATION_FORMAT,
-            CompressionFormat.mixed_precision,
-            CompressionFormat.dense,
-        ):
-            for scheme in self.config_groups.values():
-                if scheme.format is None:
-                    scheme.format = self.format
-
     def to_dict(self):
         # for compatibility with HFQuantizer
         return self.model_dump()
