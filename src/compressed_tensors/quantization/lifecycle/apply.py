807a15f42d72d521aa1c2ae474f253889db1bc1f
Sawyer Bowerman <sbowerma@redhat.com>
2026-07-29 08:01:57 -0400

    Update apply.py to address warnings versus errors
    Co-authored-by: Kyle Sayers <kylesayrs@gmail.com>
    Signed-off-by: Sawyer Bowerman <sbowerma@redhat.com>

--- src/compressed_tensors/quantization/lifecycle/apply.py
+++ src/compressed_tensors/quantization/lifecycle/apply.py
@@ -169,7 +169,7 @@ def _apply_kv_cache_scheme(
     status: QuantizationStatus,
 ):
     if not kv_cache_scheme.symmetric:
-        raise ValueError("vLLM does not support asymmetric kv cache quantization")
+        logger.warning("vLLM does not support asymmetric kv cache quantization")
 
     # applies and initializes kv cache quantization
     # this step cannot come after attention apply/initialize
