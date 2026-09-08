b06a72b01f5cab465ef65af99fbc5a42cf5e8f75
Roderick Wu <Roderick-Wu@h100-03.nemg-001.lab.rdu2.dc.redhat.com>
2026-09-08 16:36:03 -0400

    removed for real
    Signed-off-by: Roderick Wu <Roderick-Wu@h100-03.nemg-001.lab.rdu2.dc.redhat.com>

--- src/compressed_tensors/quantization/quant_args.py
+++ src/compressed_tensors/quantization/quant_args.py
@@ -149,15 +149,10 @@ class ActivationOrdering(Aliasable, str, Enum):
     # aliases
     STATIC = "static"
 
-    # Dynamic activation ordering is deprecated
-    GROUP = "group"
-    DYNAMIC = "dynamic"
-
     @staticmethod
     def get_aliases() -> dict[str, str]:
         return {
             "static": "weight",
-            "dynamic": "group",
         }
 
 
