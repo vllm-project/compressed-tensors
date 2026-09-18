5cbaee776051d0b2d80bc70b75db5e983322552a
Kyle Sayers <kylesayrs@gmail.com>
2026-09-18 17:08:18 +0000

    fix style
    Signed-off-by: Kyle Sayers <kylesayrs@gmail.com>

--- src/compressed_tensors/quantization/quant_metadata.py
+++ src/compressed_tensors/quantization/quant_metadata.py
@@ -3,8 +3,8 @@
 
 from enum import Enum
 
-from compressed_tensors.offload.module import unwrap_offload_forward
 from compressed_tensors.offload import disable_onloading
+from compressed_tensors.offload.module import unwrap_offload_forward
 from torch.nn import Module
 
 
